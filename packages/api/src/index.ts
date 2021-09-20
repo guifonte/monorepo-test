76d95b9e51778fdd76352339ea742fd50ae5f6d7
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-09-20 17:39:16 -0300

    chore(all): initial commit

---
+++ packages/api/src/index.ts
@@ -0,0 +1,3 @@
+import {Category, Device} from '@monorepo-test/core'
+
+const cat = Category(1, 'bla')
---
+++ packages/core/src/device.ts
@@ -0,0 +1,8 @@
+import { Category } from ".";
+
+export type Device = {
+    id: number
+    partNumber: number
+    color: string
+    category: Category
+}
